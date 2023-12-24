df468d4fc9a57044bdabc476c3b8bc4db4720b98
tomikartemik <artemules@mail.ru>
2023-12-24 20:32:38 +0300

    databases added

--- main.py
+++ main.py
@@ -1,15 +1,190 @@
 from fastapi import FastAPI
+import uvicorn
+from fastapi.routing import APIRouter
+from sqlalchemy import Column, Boolean, String, Integer, Float, ForeignKey, DateTime, func
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+import settings
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+import re
+from fastapi import HTTPException
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import validator
 
-app = FastAPI()
+
+##############################################
+# BLOCK FOR COMMON INTERACTION WITH DATABASE #
+##############################################
 
 
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
+# create async engine for interaction with database
+engine = create_async_engine(settings.REAL_DATABASE_URL, future=True, echo=True)
+
+# create session for the interaction with database
+async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
+##############################
+# BLOCK WITH DATABASE MODELS #
+##############################
 
-#uvicorn main:app --reload
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(String, nullable=False)
+    surname = Column(String, nullable=False)
+    number = Column(String, nullable=False, unique=True)
+    email = Column(String, nullable=False, unique=True)
+    subscribed = Column(Boolean(), nullable=False)
+
+    # Связь с таблицей orders
+    orders = relationship("Order", back_populates="user")
+
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    order_id = Column(UUID(as_uuid=True), nullable=False,  primary_key=True, default=uuid.uuid4)
+    total_amount = Column(Float, nullable=False)
+    created_at = Column(DateTime(timezone=True), nullable=False, server_default=func.now())  # Время создания заказа
+    status = Column(String, nullable=False)
+
+
+    products = relationship("Product", back_populates="order")
+
+    user_id = Column(UUID, ForeignKey('users.user_id'))
+    user = relationship("User", back_populates="orders")
+
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    product_id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String, nullable=False)
+    price = Column(Float, nullable=False)
+    sex = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    remain = Column(Integer, nullable=False)
+    size = Column(String, nullable=False)
+
+    # Связь с таблицей orders
+    order = relationship("Order", back_populates="products")
+
+
+
+###########################################################
+# BLOCK FOR INTERACTION WITH DATABASE IN BUSINESS CONTEXT #
+###########################################################
+
+
+class UserDAL:
+    """Data Access Layer for operating user info"""
+    def __init__(self, db_session: AsyncSession):
+        self.db_session = db_session
+
+    async def create_user(
+        self, name: str, surname: str, email: str
+    ) -> User:
+        new_user = User(
+            name=name,
+            surname=surname,
+            email=email,
+        )
+        self.db_session.add(new_user)
+        await self.db_session.flush()
+        return new_user
+
+
+#########################
+# BLOCK WITH API MODELS #
+#########################
+
+
+LETTER_MATCH_PATTERN = re.compile(r"^[а-яА-Яa-zA-Z\-]+$")
+
+class TunedModel(BaseModel):
+    class Config:
+        """tells pydantic to convert even non dict obj to json"""
+
+        orm_mode = True
+
+
+class ShowUser(TunedModel):
+    user_id: uuid.UUID
+    name: str
+    surname: str
+    email: EmailStr
+    is_active: bool
+
+
+class UserCreate(BaseModel):
+    name: str
+    surname: str
+    email: EmailStr
+
+    @validator("name")
+    def validate_name(cls, value):
+        if not LETTER_MATCH_PATTERN.match(value):
+            raise HTTPException(
+                status_code=422, detail="Name should contains only letters"
+            )
+        return value
+
+    @validator("surname")
+    def validate_surname(cls, value):
+        if not LETTER_MATCH_PATTERN.match(value):
+            raise HTTPException(
+                status_code=422, detail="Surname should contains only letters"
+            )
+        return value
+
+
+#########################
+# BLOCK WITH API ROUTES #
+#########################
+
+# create instance of the app
+app = FastAPI(title="luchanos-oxford-university")
+
+user_router = APIRouter()
+
+
+async def _create_new_user(body: UserCreate) -> ShowUser:
+    async with async_session() as session:
+        async with session.begin():
+            user_dal = UserDAL(session)
+            user = await user_dal.create_user(
+                name=body.name,
+                surname=body.surname,
+                email=body.email,
+            )
+            return ShowUser(
+                user_id=user.user_id,
+                name=user.name,
+                surname=user.surname,
+                email=user.email,
+                is_active=user.is_active,
+            )
+
+
+@user_router.post("/", response_model=ShowUser)
+async def create_user(body: UserCreate) -> ShowUser:
+    return await _create_new_user(body)
+
+# create the instance for the routes
+main_api_router = APIRouter()
+
+# set routes to the app instance
+main_api_router.include_router(user_router, prefix="/user", tags=["user"])
+app.include_router(main_api_router)
+
+if __name__ == "__main__":
+    # run app on the host and port
+    uvicorn.run(app, host="0.0.0.0", port=8000)
